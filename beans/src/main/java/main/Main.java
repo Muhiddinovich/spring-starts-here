43fc3cc78176f91e12a9feb0b7b7770803cb4c1b
NestOne Office <mullagoziev@gmail.com>
2026-05-26 03:30:31 -0400

    getBean() from spring context

--- beans/src/main/java/main/Main.java
+++ beans/src/main/java/main/Main.java
@@ -2,11 +2,14 @@ package main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import config.ProjectConfig;
+
 public class Main {
 
 	public static void main(String[] args) {
-		var context = new AnnotationConfigApplicationContext();
-		Parrot parrot = new Parrot();
+		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		Parrot p = context.getBean(Parrot.class);
+		System.out.println(p.getName());
 	}
 
 }
